6822df588ab79553b20c2230e6b062b6c9f5c218
ChillBunny <45990301+ChillBunny@users.noreply.github.com>
2024-03-28 08:10:13 -0400

    commit

---
+++ lib/views/buyers/main_screen.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+          unselectedItemColor: Colors.black,
+          selectedItemColor: Colors.yellow.shade900,
+          items: [
+            const BottomNavigationBarItem(
+              icon: Icon(CupertinoIcons.home),
+              label: 'HOME',
+            ),
+            BottomNavigationBarItem(
+              icon: SvgPicture.asset('assets/icons/explore.svg'),
+              label: 'CATEGORIES',
+            ),
+            BottomNavigationBarItem(
+              icon: SvgPicture.asset('assets/icons/shop.svg'),
+              label: 'STORE',
+            ),
+            BottomNavigationBarItem(
+              icon: SvgPicture.asset('assets/icons/cart.svg'),
+              label: 'CART',
+            ),
+            BottomNavigationBarItem(
+              icon: SvgPicture.asset('assets/icons/search.svg'),
+              label: 'SEARCH',
+            ),
+            BottomNavigationBarItem(
+              icon: SvgPicture.asset('assets/icons/account.svg'),
+              label: 'ACCOUNT',
+            ),
+          ]),
+    );
+  }
+}
